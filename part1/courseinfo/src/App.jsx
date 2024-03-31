1bedbb08e04e5af661044f0668258da79fa86aa1
Sai Sree <kottam.saisree@gmail.com>
2024-03-31 08:54:07 -0500

    add ex1.1-1.2

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,48 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Part part={parts[0].part} exercises={parts[0].exercises} />
+      <Part part={parts[1].part} exercises={parts[1].exercises} />
+      <Part part={parts[2].part} exercises={parts[2].exercises} />
+    </div>
+  );
+};
+const Total = (props) => {
+  const total = props.parts.reduce(
+    (sum, part) => sum + Number(part.exercises),
+    0
+  );
+  return <p>Number of exercises {total}</p>;
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const parts = [
+    { part: "Fundamentals of React", exercises: "10" },
+    { part: "Using props to pass data", exercises: "7" },
+    { part: "State of a component", exercises: "14" },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+export default App;
